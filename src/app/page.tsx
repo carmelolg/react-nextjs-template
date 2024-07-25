9e8fcc495b8c7011a94c012d486d434eddb0aa3d
carmelolg <carmelolagamba@hotmail.it>
2024-07-25 14:53:43 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,26 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./../styles/page.module.css";
 
 export default function Home() {
+
+  var title = 'This is what a React project using Next.js'
+  var subtitle = 'looks like!'
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          {title}&nbsp;<code className={styles.code}>{subtitle}</code>
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
 
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="/home.svg"
           alt="Next.js Logo"
-          width={180}
-          height={37}
+          width={500}
+          height={500}
           priority
         />
       </div>
